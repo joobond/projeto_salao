c95140b61b6776403d1d491f8177d360af5fe767
Joao Bond <jobond46@gmail.com>
2019-04-05 21:40:37 -0400

    corrigindo conflitos

--- projeto/salao/urls.py
+++ projeto/salao/urls.py
@@ -1,13 +1,36 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 from . import views
+
+# URL para Clientes
 
 app_name = "cliente"
-
-urlpatterns =[
+cliente_patterns =[
     #path('', views.index, name='index'),
     path('cliente/<int:pk>/', views.DetailViewCliente.as_view(), name='detalhes_cliente'),
     path('cliente/incluir/', views.IncluirCliente, name='incluir_cliente'),
     #path('cliente/listar', views.ListarClientes, name='listar_cliente'),
+]
+
+# URL para Serviço
+
+app_name = "servico"
+servico_patterns =[
+    path('servico/<int:pk>/', views.DetailViewServico.as_view(), name='detalhes_servico'),
+    path('servico/incluir/', views.IncluirServico, name='incluir_Servico'),
+]
+
+# URL para Produto
+
+app_name = "produto"
+produto_patterns =[
+    path('produto/<int:pk>/', views.DetailViewProduto.as_view(), name='detalhes_produto'),
+    path('produto/incluir/', views.IncluirProduto, name='incluir_produto'),
+]
+
+urlpatterns = [
+    path('', include(cliente_patterns)),
+    path('', include(produto_patterns)),
+    path('', include(servico_patterns)),
 ]
--- projeto/salao/views.py
+++ projeto/salao/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from salao.models import Cliente, Servico, Produto
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
-from .forms import ClienteForm
+from .forms import ClienteForm, ProdutoForm, ServicoForm
+from salao.models import Cliente, Servico, Produto
 
 
 class DetailViewCliente(generic.DetailView):
     model = Cliente
     template_name = 'salao/cliente/detalhes.html'
 
+class DetailViewServico(generic.DetailView):
+    model = Servico
+    template_name = 'salao/servico/detalhes.html'
+
+class DetailViewProduto(generic.DetailView):
+    model = Produto
+    template_name = 'salao/produto/detalhes.html'
 
 def IncluirCliente(request):
     template_name = 'salao/cliente/incluir.html'
@@ -20,4 +27,26 @@ def IncluirCliente(request):
         return redirect("cliente:detalhes_cliente", pk=cliente.pk)
     else:
         form = ClienteForm()
+        return render(request, template_name, {'form': form})
+
+def IncluirProduto(request):
+    template_name = 'salao/produto/incluir.html'
+    if request.method == "POST":
+        form = ProdutoForm(request.POST)
+        produto = form.save(commit=False)
+        produto.save()
+        return redirect("produto:detalhes_produto", pk=produto.pk)
+    else:
+        form = ProdutoForm()
+        return render(request, template_name, {'form': form})
+
+def IncluirServico(request):
+    template_name = 'salao/servico/incluir.html'
+    if request.method == "POST":
+        form = ServicoForm(request.POST)
+        servico = form.save(commit=False)
+        servico.save()
+        return redirect("servico:detalhes_servico", pk=servico.pk)
+    else:
+        form = ServicoForm()
         return render(request, template_name, {'form': form})
